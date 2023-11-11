803ba577649525c7b2d1d4ea18c6644f674bd1ea
Santan21 <lesliegyamfi02@gmail.com>
2023-11-11 20:50:51 +0000

    testing "does not equal" and "not" operators

--- TestSite1/app.js
+++ TestSite1/app.js
@@ -16,7 +16,12 @@ let fourthVariable = 'Hello' + 'world';
 console.log(fourthVariable);
 
 //testing strict equality between variables
-letsixthvariable = 4
-sixthVariable == 3
+letfifthVariable = 4
+fifthVariable == 3
 // this will return a boolean (false) result. 
 // == tests if too data types or values are equal and returns a boolean
+
+let sixthVariable = 9
+!(sixthVariable) === 9
+// this will return a boolean (false) result. ! means "not equal to", 
+// this makes it a a false statement as the values of the variables are equal
