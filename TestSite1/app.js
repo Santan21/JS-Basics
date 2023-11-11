92d2e0fe670dd18f6aa1122234a9fd4628dba922
Santan21 <lesliegyamfi02@gmail.com>
2023-11-11 19:49:55 +0000

    experimenting data types stored in variables

--- TestSite1/app.js
+++ TestSite1/app.js
@@ -1,6 +1,10 @@
  //experimenting variables
- let myVariable = 'Leslie';
+let myVariable = 'Leslie';
 console.log(myVariable);
 
 //experimenting data types that can be stored in variables
 let newVariable = [1, 10, 'Bob', 'Jessie']
+let nextVariable = true;
+let firstVariable = document.querySelectorAll('h1')
+let secondVariable = 'Leslie';
+
