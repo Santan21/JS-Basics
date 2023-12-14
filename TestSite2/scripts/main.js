18a9f81e28fe4d7368baa1bce9b635e989da6eb9
Santan21 <lesliegyamfi02@gmail.com>
2023-12-14 19:56:11 +0000

    finished testing

--- TestSite2/scripts/main.js
+++ TestSite2/scripts/main.js
@@ -1,9 +1,9 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "JS Basics";
+const buttonA = document.querySelector("#button_A");
+const headingA = document.querySelector("#heading_A");
+
 
 buttonA.onclick = () => {
     const name = prompt("What is your name?");
     alert('Hello, ${name}, nice to meet you');
-
-    
+    headingA.textContent = `Welcome ${name};`  
 } ;
