12d9fcb2687e3f3623d904ac51c33a908ec613aa
Santan21 <lesliegyamfi02@gmail.com>
2023-12-14 20:18:13 +0000

    resolved error

--- TestSite2/scripts/main.js
+++ TestSite2/scripts/main.js
@@ -4,5 +4,5 @@ const headingA = document.querySelector("#heading_A");
 buttonA.onclick = () => {
     const name = prompt("What is your name?");
     alert(`Hello, ${name}, nice to meet you`);
-    headingA.textContent = `Welcome ${name};`  
+    headingA.textContent = `Welcome ${name}`  
 };
